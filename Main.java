c4909981ba79d849f3b11860ebd76741c43f321f
Magimi <36344269+Magimi@users.noreply.github.com>
2018-04-10 21:50:30 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,35 +1,26 @@
-import controller.PaneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-
-
-
 public class Main extends Application {
 
     public static void main(String[] args) {
-
         launch(args);
-
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(this.getClass().getResource("fxml/Calc.fxml"));
-        PaneController controller;
-        controller = new PaneController();
-        loader.setController(controller);
+        loader.setLocation(getClass().getResource("fxml/Calc.fxml"));
         Pane pane = loader.load();
+
         Scene scene = new Scene(pane);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Najlepszy kalkulator");
         primaryStage.show();
-
-
+        primaryStage.setResizable(false);
     }
 }
